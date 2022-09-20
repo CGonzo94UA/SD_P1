471cbdc8ca606a46fade37ac377727792fbc7d09
CGonzo94UA <113585473+CGonzo94UA@users.noreply.github.com>
2022-09-20 09:55:16 +0200

    Corregidas trazas

--- SocketsChatRoom/src/Cliente.java
+++ SocketsChatRoom/src/Cliente.java
@@ -9,126 +9,140 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 // Client class
 public class Cliente{
-	
-	private Socket socket;
-	private BufferedReader bReader;
-	private BufferedWriter bWriter;
-	
-	private String username;
-	
-	// Constructor
-	public Cliente(Socket socket, String username){
-		try {
-			this.socket = socket;
-			
-			//Como se quieren enviar mensajes de caracteres se usa StreamWriter
-			this.bWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			
-			this.bReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			this.username = username;
-			
-		}catch(IOException e) {
-			this.closeConnections();
-		}
-		
-	}
-	
-	public void enviaMensaje(){
-		try{
-			this.bWriter.write(this.username);
-			this.bWriter.newLine();
-			this.bWriter.flush();
-			
-			Scanner sc = new Scanner(System.in);
-			
-			while(this.socket.isConnected()) {
-				String mensaje = sc.nextLine();
-				this.bWriter.write(this.username + ": "+ mensaje);
-				this.bWriter.newLine();
-				this.bWriter.flush();
-			}
-			
-			sc.close();
-			
-		}catch(IOException e) {
-			this.closeConnections();
-		}
-	}
-	
-	public void recibeMensajes(){
-		//Se utiliza hilos para no tener bloqueada a la app
-		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				String mensajeChat;
-				
-				while(socket.isConnected()) {
-					try {
-						mensajeChat = bReader.readLine();
-						System.out.println(mensajeChat);
-					}catch(IOException e) {
-						closeConnections();
-					}
-				}
-			}
-		}).start();;
-	}
-	
-	public void closeConnections(){
-		
-		try {
-			if(this.socket != null) {
-				this.socket.close();
-			}
-			
-			if(this.bReader != null) {
-				this.bReader.close();
-			}
-			
-			if(this.bWriter != null) {
-				this.bWriter.close();
-			}
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	
-	public static void main(String[] args) throws IOException{
-		try	{
-			
-			String host;
-			String puerto;
-			if (args.length < 2) {
-				System.out.println ("Debe indicar la direccion del servidor y el puerto");
-				System.out.println ("$./Cliente nombre_servidor puerto_servidor");
-				System.exit(-1);
-			}
-			host = args[0];
-			puerto = args[1];
-			
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Introduce tu nombre de usuario: ");
-			String username = sc.nextLine();
-				
-			// Se establece la conexion con el servidor y con el puerto indicado por el cliente
-			Socket s = new Socket(host, Integer.parseInt(puerto));
-			
-			Cliente cliente = new Cliente(s, username);
-			//Se ejecutan como hilos separados, por eso no se bloquean
-			cliente.recibeMensajes();
-			cliente.enviaMensaje();
-			
-			sc.close();
-	
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+
+    private Socket socket;
+    private BufferedReader bReader;
+    private BufferedWriter bWriter;
+
+    private String username;
+
+    DateFormat fordate = new SimpleDateFormat("dd/MM/yyyy");
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
+
+    // Constructor
+    public Cliente(Socket socket, String username){
+        try {
+            this.socket = socket;
+
+            //Como se quieren enviar mensajes de caracteres se usa StreamWriter
+            this.bWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            this.bReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+
+        }catch(IOException e) {
+            this.closeConnections(socket, bReader, bWriter);
+        }
+
+    }
+
+    public void enviaMensaje(){
+        try{
+            this.bWriter.write(this.username);
+            this.bWriter.newLine();
+            this.bWriter.flush();
+
+            Scanner sc = new Scanner(System.in);
+
+            while(this.socket.isConnected()) {
+                StringBuilder trazaFechaHora = new StringBuilder();
+
+                Date date = new Date();
+                trazaFechaHora.append(fordate.format(date) + "\t");
+                trazaFechaHora.append(fortime.format(date)+"  ||  ");
+                this.bWriter.write(trazaFechaHora.toString());
+
+                String mensaje = sc.nextLine();
+
+                this.bWriter.write(this.username + ": "+ mensaje);
+                this.bWriter.newLine();
+                this.bWriter.flush();
+            }
+
+            sc.close();
+
+        }catch(IOException e) {
+            this.closeConnections(socket, bReader, bWriter);
+        }
+    }
+
+    public void recibeMensajes(){
+        //Se utiliza hilos para no tener bloqueada a la app
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String mensajeChat;
+
+                while(socket.isConnected()) {
+                    try {
+                        mensajeChat = bReader.readLine();
+                        System.out.println(mensajeChat);
+                    }catch(IOException e) {
+                        closeConnections(socket, bReader, bWriter);
+                    }
+                }
+            }
+        }).start();;
+    }
+
+    public void closeConnections(Socket socket, BufferedReader bReader, BufferedWriter bWriter){
+
+        try {
+            if(this.socket != null) {
+                socket.close();
+            }
+
+            if(this.bReader != null) {
+                bReader.close();
+            }
+
+            if(this.bWriter != null) {
+                bWriter.close();
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void main(String[] args) throws IOException{
+        try	{
+
+            String host;
+            String puerto;
+            if (args.length < 2) {
+                System.out.println ("Debe indicar la direccion del servidor y el puerto");
+                System.out.println ("$./Cliente nombre_servidor puerto_servidor");
+                System.exit(-1);
+            }
+            host = args[0];
+            puerto = args[1];
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introduce tu nombre de usuario: ");
+            String username = sc.nextLine();
+
+            // Se establece la conexion con el servidor y con el puerto indicado por el cliente
+            Socket s = new Socket(host, Integer.parseInt(puerto));
+
+            Cliente cliente = new Cliente(s, username);
+            //Se ejecutan como hilos separados, por eso no se bloquean
+            cliente.recibeMensajes();
+            cliente.enviaMensaje();
+
+            sc.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
--- SocketsChatRoom/src/Server.java
+++ SocketsChatRoom/src/Server.java
@@ -3,188 +3,183 @@
  * Clase que implementa el lado del Servidor, contiene dos clases: Servidor y ClientHandler
  * El servidor establece la conexion a traves del socket y queda en espera aceptando conexiones, obtiene los inputs
  * procedentes de las peticiones realizadas por los clientes, crea los objetos ClientHandler y crea los hilos
- * 
+ *
  */
 
 import java.io.*;
 import java.util.*;
 import java.net.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 // Server class
 public class Server{
-	
-	//Clase especifica de Java para sockets de servidores
-	private ServerSocket serverSocket;
-     
+
+    //Clase especifica de Java para sockets de servidores
+    private ServerSocket serverSocket;
+
     public Server(ServerSocket sSocket) {
-    	this.serverSocket = sSocket;
+        this.serverSocket = sSocket;
     }
-    
+
     //Funcion para iniciar el servidor y que se mantenga escuchando para aceptar nuevos clientes
     public void startServer(){
-    	try {
-    		//Mientras no cerremos el socket del servidor, se aceptaran nuevos clientes
-    		while(!serverSocket.isClosed()) {
-    			Socket nClienteSocket = serverSocket.accept();
-    			System.out.println("Se ha conectado un nuevo cliente: " + nClienteSocket);
+        try {
+            //Mientras no cerremos el socket del servidor, se aceptaran nuevos clientes
+            while(!serverSocket.isClosed()) {
+                Socket nClienteSocket = serverSocket.accept();
+                System.out.println("Se ha conectado un nuevo cliente: " + nClienteSocket);
 
-				// Se crea un ClientHandler para el nuevo cliente
-	            ClientHandler nClientHand = new ClientHandler(nClienteSocket);
-	 
-	            // Se crea nuevo hilo
-	            Thread t = new Thread(nClientHand);
-	            //Se inicia el hilo
-		        t.start();
+                // Se crea un ClientHandler para el nuevo cliente
+                ClientHandler nClientHand = new ClientHandler(nClienteSocket);
 
-    		}
-    	}catch (IOException e){
-			this.closeServer();
-    	}
+                // Se crea nuevo hilo
+                Thread t = new Thread(nClientHand);
+                //Se inicia el hilo
+                t.start();
+
+            }
+        }catch (IOException e){
+            this.closeServer();
+        }
     }
-    
+
     public void closeServer() {
-    	try {
-    		if(serverSocket != null) {
-    			serverSocket.close();
-    		}
-    	}catch (IOException e){
-			e.printStackTrace();
-    	}
+        try {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
-       
-    
-	public static void main(String[] args) throws IOException{
-		
-		//Puerto de escucha
-		String puerto="";
 
-		try{
-			
-			if (args.length < 1) {
-				System.out.println("Debe indicar el puerto de escucha del servidor");
-				System.out.println("$./Servidor puerto_servidor");
-				System.exit (1);
-			}
-			puerto = args[0];
-			ServerSocket serverSocket = new ServerSocket(Integer.parseInt(puerto));
-			Server servidor = new Server(serverSocket);
-		    System.out.println("Servidor escucha el puerto " + puerto);
-		    servidor.startServer();
-	
-		}catch(Exception e){
-			System.out.println("Error: " + e.toString());
-		}
-		
-	}
+
+    public static void main(String[] args) throws IOException{
+
+        //Puerto de escucha
+        String puerto="";
+
+        try{
+
+            if (args.length < 1) {
+                System.out.println("Debe indicar el puerto de escucha del servidor");
+                System.out.println("$./Servidor puerto_servidor");
+                System.exit (1);
+            }
+            puerto = args[0];
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(puerto));
+            Server servidor = new Server(serverSocket);
+            System.out.println("Servidor escucha el puerto " + puerto);
+            servidor.startServer();
+
+        }catch(Exception e){
+            System.out.println("Error: " + e.toString());
+        }
+
+    }
 }
 
 // ClientHandler class
 class ClientHandler implements Runnable{
-	
-	DateFormat fordate = new SimpleDateFormat("dd/MM/yyyy");
-	DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
-	
-	// Vector de clientes activos. Se utiliza estructura vector porque es sincronizada y 
-	//solo un hilo puede acceder cada vez
-	static Vector<ClientHandler> activeClients = new Vector<ClientHandler>();
-	
-	private String clientUsername;
-	private Socket s;
-	//Para leer mensajes
-	private BufferedReader bReader;
-	//Para enviar mensajes
-	private BufferedWriter bWriter;
-	
 
-	// Constructor
-	public ClientHandler(Socket s){
-		try {
-			this.s = s;
-			
-			//Como se quieren enviar mensajes de caracteres se usa StreamWriter
-			this.bWriter = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-			
-			this.bReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			this.clientUsername = bReader.readLine();
-			
-			ClientHandler.activeClients.add(this);
-			this.broadcastMessage("SERVIDOR: "+ this.clientUsername + " se ha conectado al chat!");
-			
-		}catch(IOException e) {
-			this.closeConnections();
-		}
-		
-	}
+    // Vector de clientes activos. Se utiliza estructura vector porque es sincronizada y
+    //solo un hilo puede acceder cada vez
+    static Vector<ClientHandler> activeClients = new Vector<ClientHandler>();
 
-	@Override
-	public void run(){
-		
-		StringBuilder mensaje = new StringBuilder();
-		//Mientras el cliente siga conectado se sigue esperando mensajes
-		while (s.isConnected()){
-			
-			try {
-				
-				Date date = new Date();
-				mensaje.append(fordate.format(date) + "\t");
-				mensaje.append(fortime.format(date)+"  ||  ");
-				
-				//Se realiza aqui para evitar que la app este parada esperando
-				mensaje.append(bReader.readLine());
-				//Se envia el mensaje al resto de clientes
-				this.broadcastMessage(mensaje.toString());
-				
-			} catch (IOException e) {
-				this.closeConnections();
-				//para salir del bucle
-				break;
-			}
-		}
-		
-	}
-	
-	public void broadcastMessage(String mensaje) {
-		
-		//Se itera por todo el vector que almacena los clientes
-		for(ClientHandler clientHand: activeClients){
-			try{
-				//Si el nombre de usuario del cliente es diferente al actual se envia mensaje, si no no
-				if(!clientHand.clientUsername.equals(this.clientUsername)){
-					clientHand.bWriter.write(mensaje);
-					//el mensaje acaba con intro
-					clientHand.bWriter.newLine();
-					clientHand.bWriter.flush();
-				}
-			}catch(IOException e) {
-				this.closeConnections();
-			}
-		}
-	}
-	
-	public void eliminaClientHandler(){
-		ClientHandler.activeClients.remove(this);
-		this.broadcastMessage("SERVIDOR: "+ this.clientUsername + " se ha desconectado del chat!");
-	}
-	
-	public void closeConnections() {
-		this.eliminaClientHandler();
-		try {
-			if(this.s != null) {
-				this.s.close();
-			}
-			
-			if(this.bReader != null) {
-				this.bReader.close();
-			}
-			
-			if(this.bWriter != null) {
-				this.bWriter.close();
-			}
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
+    private String clientUsername;
+    private Socket s;
+    //Para leer mensajes
+    private BufferedReader bReader;
+    //Para enviar mensajes
+    private BufferedWriter bWriter;
+
+
+    // Constructor
+    public ClientHandler(Socket s){
+        try {
+            this.s = s;
+
+            //Como se quieren enviar mensajes de caracteres se usa StreamWriter
+            this.bWriter = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+
+            this.bReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            this.clientUsername = bReader.readLine();
+
+            ClientHandler.activeClients.add(this);
+            this.broadcastMessage("SERVIDOR: "+ this.clientUsername + " se ha conectado al chat!");
+
+        }catch(IOException e) {
+            this.closeConnections(s, bReader, bWriter);
+        }
+
+    }
+
+    /*
+     * Se usa un hilo para recibir los mensajes y otro para seguir escuchando al chat, asi no
+     * se para la app esperando la recepcion de mensajes
+     */
+    @Override
+    public void run(){
+
+        String mensaje;
+        //Mientras el cliente siga conectado se sigue esperando mensajes
+        while (s.isConnected()){
+
+            try{
+
+                //Se realiza aqui para evitar que la app este parada esperando
+                mensaje = bReader.readLine();
+                //Se envia el mensaje al resto de clientes
+                this.broadcastMessage(mensaje);
+
+            } catch (IOException e) {
+                this.closeConnections(s, bReader, bWriter);
+                //para salir del bucle
+                break;
+            }
+        }
+
+    }
+
+    public void broadcastMessage(String mensaje) {
+
+        //Se itera por todo el vector que almacena los clientes
+        for(ClientHandler clientHand : activeClients){
+            try{
+                //Si el nombre de usuario del cliente es diferente al actual se envia mensaje, si no no
+                if(!this.clientUsername.equals(clientHand.clientUsername)){
+                    clientHand.bWriter.write(mensaje);
+                    //el mensaje acaba con intro
+                    clientHand.bWriter.newLine();
+                    clientHand.bWriter.flush();
+                }
+            }catch(IOException e) {
+                this.closeConnections(s, bReader, bWriter);
+            }
+        }
+    }
+
+    public void eliminaClientHandler(){
+        ClientHandler.activeClients.remove(this);
+        this.broadcastMessage("SERVIDOR: "+ this.clientUsername + " se ha desconectado del chat!");
+    }
+
+    public void closeConnections(Socket s, BufferedReader bReader, BufferedWriter bWriter) {
+        this.eliminaClientHandler();
+        try {
+            if(s != null) {
+                s.close();
+            }
+
+            if(bReader != null) {
+                bReader.close();
+            }
+
+            if(bWriter != null) {
+                bWriter.close();
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
